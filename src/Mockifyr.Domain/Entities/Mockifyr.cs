6b8511c7756993e04dba64a4f69ca3c257ee7ba4
celikomr <celikomr@outlook.com>
2024-05-28 20:58:18 +0300

    feat: Created Mockifyr entity

---
+++ src/Mockifyr.Domain/Entities/Mockifyr.cs
@@ -0,0 +1,11 @@
+﻿using Mockifyr.Domain.Common;
+
+namespace Mockifyr.Domain.Entities;
+
+public class Mockifyr : BaseAuditableEntity
+{
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public int MockifyrRequestId { get; set; }
+    public int MockifyrResponseId { get; set; }
+}
